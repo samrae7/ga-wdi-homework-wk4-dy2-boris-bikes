daa849994b23f0607969d5de88993ef744fdd26e
Jeremy Marer <jeremymarer@gmail.com>
2015-09-01 14:24:04 +0100

    dockstation test for docking a bike

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/docking_station'
+require_relative '../lib/bike'
+
+describe DockingStation do
+
+  it 'should be empty after we build it' do
+    station = DockingStation.new
+    
+    expect(station.bike_count).to eq 0
+  end
+
+  it 'should be able to dock a bike' do
+    station = DockingStation.new
+    bike = Bike.new
+    station.dock(bike)
+    
+    expect(station.bike_count).to eq 1
+  end
+
+end

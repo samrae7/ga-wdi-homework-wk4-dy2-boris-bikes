36574ec0a90102f05531b034a42636610c04f9f5
Sam Rae <samuelrae@gmail.com>
2015-09-02 09:33:13 +0100

    refactored garage tests

--- spec/garage_spec.rb
+++ spec/garage_spec.rb
@@ -6,38 +6,36 @@ describe Garage do
   let(:garage) {Garage.new (25)}
   let(:van) {Van.new (15)}
 
+  def put_broken_bikes_in_garage(bike, van)
+    bike.break
+    van.accept_bike bike
+    garage.accept_bike_from_van(bike,van)
+  end
+
   it 'should be empty after we instantiate it' do
     expect(garage.bike_count).to eq 0
     
   end
 
   it 'should increase its count of bikes when it accepts bikes from the van' do
-    bike.break
-    van.accept_bike bike
-    garage.accept_bike_from_van(bike,van)
+    put_broken_bikes_in_garage(bike, van)
     expect(garage.bike_count).to eq 1
   end
 
   it 'should reduce number of bikes in the van by one when it accepts a bike from the van' do
-    bike.break
-    van.accept_bike bike
-    garage.accept_bike_from_van(bike, van)
+    put_broken_bikes_in_garage(bike, van)
     expect(van.bike_count).to eq 0
   end
 
   it 'should decrease its count of bikes when it releases one to the van' do
-    bike.break
-    van.accept_bike bike
-    garage.accept_bike_from_van(bike, van)
+    put_broken_bikes_in_garage(bike, van)
     bike.fix
     garage.release_bike_to_van(bike, van)
     expect(garage.bike_count).to eq 0
   end
 
   it 'should increase number of bikes in the van when it releases a bike back to the van' do
-    bike.break
-    van.accept_bike bike
-    garage.accept_bike_from_van(bike, van)
+    put_broken_bikes_in_garage(bike, van)
     bike.fix
     garage.release_bike_to_van(bike,van)
     expect(van.bike_count).to eq 1
@@ -51,12 +49,10 @@ describe Garage do
   it 'should stop accepting bikes when it is full' do
       25.times do
         bike = Bike.new
-        bike.break
-        garage.accept_bike_from_van(bike, van)
+        put_broken_bikes_in_garage(bike, van)
       end
       expect do
-        bike.break
-        garage.accept_bike_from_van(bike, van)
+        put_broken_bikes_in_garage(bike, van)
       end.to raise_error 'Garage is full'
   end
 
@@ -65,12 +61,10 @@ describe Garage do
   end
 
   it 'should only release fixed bikes' do
-    bike.break
-    van.accept_bike bike
-    garage.accept_bike_from_van(bike,van)
+    put_broken_bikes_in_garage(bike, van)
     expect{garage.release_bike_to_van(bike, van) }.to raise_error "This bike isn't fixed yet. Leave it here"
   end
 
 end
 
-# The situation with the garage is very similar. What's the difference between a garage and a docking station? Only a van is supposed to take bikes out of a garage. Also, the bikes get fixed once they get to the garage. Otherwise, the garage is not much different from the station.
+# SPEC - The situation with the garage is very similar. What's the difference between a garage and a docking station? Only a van is supposed to take bikes out of a garage. Also, the bikes get fixed once they get to the garage. Otherwise, the garage is not much different from the station.
